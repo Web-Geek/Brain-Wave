1a8bb662ee728f4268cd97613b48600b521a1d6d
Web-Geek <Web-Geek>
2021-06-07 23:48:21 +0530

    Image/Question Update functionalities

--- src/pages/Mainpage/Mainpage.jsx
+++ src/pages/Mainpage/Mainpage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useEffect, useState } from 'react'
 import MainpageCSS from './MainpageCSS.module.css'
 import firebase from '../../utils/firebase'
 
@@ -6,27 +6,13 @@ import firebase from '../../utils/firebase'
 const Mainpage = () => {
     // State for Counter
     const [counter, setCounter] = useState('')
-    const [qn, setQn] = useState('')
     const [data, setData] = useState([])
     const [images, setImages] = useState([])
-    // const [images, setImages] = useState([])
-    const [isLoaded,setIsLoaded] = useState(false);
-    const initial = useRef(true);
+
+    // loading img
+    const loading_img = "https://abucoins.com/img/trade/pre-loading.gif"
 
 
-    useEffect(() => {
-        const ref = firebase.database().ref('data').child(555);
-        ref.on('value', (snapshot) => {
-          const all = snapshot.val();
-          const array = [];
-          setData(all);
-          setImages(all.images)
-        });
-      }, []);
-
-      console.log(data);
-
-    const imgurl = "https://thumb.fakeface.rest/thumb_"
 
     function getCounter() {
         var d = new Date();
@@ -39,24 +25,59 @@ const Mainpage = () => {
         return [questionNo, remainder]
     }
 
+    useEffect(() => {
+        var qNo = getCounter()[0]
+        const ref = firebase.database().ref('data').child(qNo);
+        ref.on('value', (snapshot) => {
+            const all = snapshot.val();
+            setData(all);
+            setImages(all.images)
+        });
+    }, []);
+
+    const imgurl = "https://thumb.fakeface.rest/thumb_"
+
+
+
+    useEffect(() => {
+        setInterval(() => {
+            let  [q,remainder] = getCounter();
+            // console.log(q,remainder)
+            setCounter(remainder);
+            if (remainder === 29) {
+                var qNo = getCounter()[0]
+                const ref = firebase.database().ref('data').child(qNo);
+                ref.on('value', (snapshot) => {
+                    const all = snapshot.val();
+                    setData(all);
+                    setImages(all.images)
+                });
+            }
+        }, 1000)
+    }, [])
 
 
     // JSX
     return (
         <div className={`container ${MainpageCSS.container}`}>
             <div className={`card ${MainpageCSS.card}`}>
+
+                {/* <div className={MainpageCSS.layer}>
+                    <span className={MainpageCSS.spinner}></span>
+                </div> */}
+
                 <span className={MainpageCSS.txtspan}>
                     <h4 className={MainpageCSS.h1}>{data.question}</h4>
                 </span>
                 <div className="row">
                     <div className="col-6">
                         <div className={MainpageCSS.imgdiv}>
-                            <img src={imgurl + images[0]} alt="logo" className={MainpageCSS.img} draggable="false" />
+                            <img src={imgurl + images[0]} alt="" onError={e=>e.target.src=loading_img} className={MainpageCSS.img} draggable="false" />
                         </div>
                     </div>
                     <div className="col-6">
                         <div className={MainpageCSS.imgdiv}>
-                            {/* <img src={imgurl + images[1]} alt="logo" className={MainpageCSS.img} draggable="false" /> */}
+                            <img src={imgurl + images[1]} alt="" onError={e=>e.target.src=loading_img} className={MainpageCSS.img} draggable="false" />
                         </div>
                     </div>
                 </div>
@@ -68,12 +89,12 @@ const Mainpage = () => {
                 <div className="row mt-4">
                     <div className="col-6">
                         <div className={MainpageCSS.imgdiv}>
-                            {/* <img src={imgurl + images[2]} alt="logo" className={MainpageCSS.img} draggable="false" /> */}
+                            <img src={imgurl + images[2]} alt="" onError={e=>e.target.src=loading_img} className={MainpageCSS.img} draggable="false" />
                         </div>
                     </div>
                     <div className="col-6">
                         <div className={MainpageCSS.imgdiv}>
-                            {/* <img src={imgurl + images[3]} alt="logo" className={MainpageCSS.img} draggable="false" /> */}
+                            <img src={imgurl + images[3]} alt="" onError={e=>e.target.src=loading_img} className={MainpageCSS.img} draggable="false" />
                         </div>
                     </div>
                 </div>
