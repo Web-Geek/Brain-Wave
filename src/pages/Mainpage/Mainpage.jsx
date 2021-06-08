4b19e25b467d07789fedfebc57901b9bef2901ce
Web-Geek <Web-Geek>
2021-06-08 15:41:27 +0530

    Progress bar with percentage

--- src/pages/Mainpage/Mainpage.jsx
+++ src/pages/Mainpage/Mainpage.jsx
@@ -9,10 +9,21 @@ import loader from "../../Assets/loader.gif"
 const Mainpage = () => {
     // State for Counter
     const [counter, setCounter] = useState('')
+    const [selectedImgColor, setSelectedImgColor] = useState(["#eeeeee", "#eeeeee", "#eeeeee", "#eeeeee"])
+    const [result, setResult] = useState(false);
+    const [msg, setMsg] = useState('Brain Wave');
+    const [selectedImgNo, setSelectedImgNo] = useState('')
+
     const [data, setData] = useState([])
     const [images, setImages] = useState([])
-    const [votes,setVotes] = useState([0,0,0,0])
+    const [votes, setVotes] = useState([80, 40, 120,160])
     const [selected, setSelected] = useState(false)
+    var sum = votes.reduce(function(a, b){
+        return a + b;
+    }, 0);
+
+    // Local Storage
+    var uname = localStorage.getItem('username')
 
 
     function getCounter() {
@@ -28,7 +39,7 @@ const Mainpage = () => {
 
     useEffect(() => {
         var qNo = getCounter()[0]
-        const ref = firebase.database().ref('data').child(qNo);
+        const ref = firebase.database().ref('data').child(1);
         ref.on('value', (snapshot) => {
             const all = snapshot.val();
             setData(all);
@@ -38,6 +49,17 @@ const Mainpage = () => {
 
     const imgurl = "https://thumb.fakeface.rest/thumb_"
 
+    function updataData() {
+        var qNo = getCounter()[0]
+        const ref = firebase.database().ref('data').child(1);
+        ref.on('value', (snapshot) => {
+            const all = snapshot.val();
+            // setVotes(all.votes)
+            setData(all);
+            setImages(all.images)
+        });
+    }
+
 
 
     useEffect(() => {
@@ -45,29 +67,35 @@ const Mainpage = () => {
             let remainder = getCounter()[1];
             setCounter(remainder);
             if (remainder === 29) {
-                var qNo = getCounter()[0]
-                const ref = firebase.database().ref('data').child(qNo);
-                ref.on('value', (snapshot) => {
-                    const all = snapshot.val();
-                    setData(all);
-                    setImages(all.images)
-                });
-                setVotes([0,0,0,0])
+                setMsg('BRAIN WAVE')
+                updataData()
+                // setVotes([0, 0, 0, 0])
                 setSelected(false)
+            }
+            if (remainder === 5) {
+                setSelectedImgColor(["#eeeeee", "#eeeeee", "#eeeeee", "#eeeeee"])
+
+                // setVotes([Math.random() * 50, Math.random() * 20, Math.random() * 40, Math.random() * 80])
+                setMsg('Result')
+
             }
         }, 1000)
     }, [])
 
-   function handleClick(e){
-    if (!selected) {
-        var target = e.target
-        var img_no = target.alt
-        // setVotes(p => { p[img_no] += 1; return p })
-        var r=Math.random()
-        setVotes([Math.random()*50,Math.random()*20,Math.random()*40,Math.random()*80])
-        setSelected(true)
-    }    
-   }
+    function handleClick(e) {
+    
+        if (!selected) {
+            var target = e.target
+            var img_no = target.alt
+            setSelectedImgColor(p=>{p[img_no]="#8c7b7b"; return p})
+            setSelected(true)
+            var uname= localStorage.getItem('username')
+            setMsg('selected')
+        }
+
+    }
+
+
 
     // JSX
     return (
@@ -80,7 +108,9 @@ const Mainpage = () => {
                 <div className="row">
                     <div className="col-6">
                         <div className={MainpageCSS.imgdiv}>
-                        <CircularProgressbarWithChildren value={votes[0]}
+                            <CircularProgressbarWithChildren value={votes[0]}
+                                maxValue={sum}
+                                // minValue={Math.min(...votes)}
                                 styles={{
                                     path: {
                                         // Path color
@@ -88,29 +118,30 @@ const Mainpage = () => {
                                     },
                                     trail: {
                                         // Trail color
-                                        stroke: '#eeeeee',
+                                        stroke: selectedImgColor[0],
                                     }
                                 }}>
-                            <img src={imgurl + images[0]} alt="0" onClick={handleClick} onError={e=>e.target.src=loader} className={MainpageCSS.img} draggable="false" />
-                        </CircularProgressbarWithChildren> 
-                            
+                                <img src={imgurl + images[0]} alt="0" onClick={handleClick} onError={e => e.target.src = loader} className={MainpageCSS.img} draggable="false" />
+                            </CircularProgressbarWithChildren>
+
                         </div>
                     </div>
                     <div className="col-6">
                         <div className={MainpageCSS.imgdiv}>
                             <CircularProgressbarWithChildren value={votes[1]}
-                                    styles={{
-                                        path: {
-                                            // Path color
-                                            stroke: `#9180ff`
-                                        },
-                                        trail: {
-                                            // Trail color
-                                            stroke: '#eeeeee',
-                                        }
-                                    }}>
-                                <img src={imgurl + images[1]} alt="1" onClick={handleClick} onError={e=>e.target.src=loader} className={MainpageCSS.img} draggable="false" />
-                            </CircularProgressbarWithChildren> 
+                                maxValue={sum}
+                                styles={{
+                                    path: {
+                                        // Path color
+                                        stroke: `#9180ff`
+                                    },
+                                    trail: {
+                                        // Trail color
+                                        stroke: selectedImgColor[1],
+                                    }
+                                }}>
+                                <img src={imgurl + images[1]} alt="1" onClick={handleClick} onError={e => e.target.src = loader} className={MainpageCSS.img} draggable="false" />
+                            </CircularProgressbarWithChildren>
                         </div>
                     </div>
                 </div>
@@ -123,39 +154,41 @@ const Mainpage = () => {
                     <div className="col-6">
                         <div className={MainpageCSS.imgdiv}>
                             <CircularProgressbarWithChildren value={votes[2]}
-                                    styles={{
-                                        path: {
-                                            // Path color
-                                            stroke: `#84db92`
-                                        },
-                                        trail: {
-                                            // Trail color
-                                            stroke: '#eeeeee',
-                                        }
-                                    }}>
-                                <img src={imgurl + images[2]} alt="2" onClick={handleClick} onError={e=>e.target.src=loader} className={MainpageCSS.img} draggable="false" />
-                            </CircularProgressbarWithChildren> 
+                                maxValue={sum}
+                                styles={{
+                                    path: {
+                                        // Path color
+                                        stroke: `#84db92`
+                                    },
+                                    trail: {
+                                        // Trail color
+                                        stroke: selectedImgColor[2],
+                                    }
+                                }}>
+                                <img src={imgurl + images[2]} alt="2" onClick={handleClick} onError={e => e.target.src = loader} className={MainpageCSS.img} draggable="false" />
+                            </CircularProgressbarWithChildren>
                         </div>
                     </div>
                     <div className="col-6">
                         <div className={MainpageCSS.imgdiv}>
                             <CircularProgressbarWithChildren value={votes[3]}
-                                    styles={{
-                                        path: {
-                                            // Path color
-                                            stroke: `#f2dc5e`
-                                        },
-                                        trail: {
-                                            // Trail color
-                                            stroke: '#eeeeee',
-                                        }
-                                    }}>
-                                <img src={imgurl + images[3]} alt="3" onClick={handleClick} onError={e=>e.target.src=loader} className={MainpageCSS.img} draggable="false" />
-                            </CircularProgressbarWithChildren> 
+                                maxValue={sum}
+                                styles={{
+                                    path: {
+                                        // Path color
+                                        stroke: `#f2dc5e`
+                                    },
+                                    trail: {
+                                        // Trail color
+                                        stroke: selectedImgColor[3],
+                                    }
+                                }}>
+                                <img src={imgurl + images[3]} alt="3" onClick={handleClick} onError={e => e.target.src = loader} className={MainpageCSS.img} draggable="false" />
+                            </CircularProgressbarWithChildren>
                         </div>
                     </div>
                 </div>
-                <p className={MainpageCSS.p}>Brain Wave</p>
+                <p className={MainpageCSS.p}>{msg}</p>
             </div>
         </div>
     )
